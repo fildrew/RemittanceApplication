ad5013efe90038492015aaeec330cfd7cddc214c
fildrew <philedu30@gmail.com>
2025-02-17 22:56:16 +0100

    Initial commit

---
+++ RemittanceApplication/src/test/java/com/mastercard/RemittanceApplication/RemittanceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mastercard.RemittanceApplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RemittanceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
